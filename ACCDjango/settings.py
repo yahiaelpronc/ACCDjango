cf7d064baf196d50a0309dbc047736f11afe4930
mostafamasrya <106432717+mostafamasrya@users.noreply.github.com>
2022-07-24 13:39:33 +0200

    test

--- ACCDjango/settings.py
+++ ACCDjango/settings.py
@@ -131,10 +131,26 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'acc-react/build/static')
+    os.path.join(BASE_DIR, 'acc-react/build/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/profileImages')
+
+
+
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'react/build/static'),
+#     os.path.join(BASE_DIR, 'static'),
+#                     ]
+
+# MEDIA_URL = 'media/'
+
+# MEDIA_ROOT = BASE_DIR/'static'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
